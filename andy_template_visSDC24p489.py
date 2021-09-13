5dd222a45daa31062d95a99df258fd94ebd9c1b4
ejwatkins-astro <78483396+ejwatkins-astro@users.noreply.github.com>
2021-09-13 13:40:17 +0200

    Add files via upload

--- andy_template_visSDC24p489.py
+++ andy_template_visSDC24p489.py
@@ -1,8 +1,29 @@
 # -*- coding: utf-8 -*-
 """
+sdc24
 Template for running the visualiser. For options and help,
 on the functions and classes enter help(class_name) or help(function_name)
-for further documentation.
+for further documentation and options for on loading in the data
+
+NOTES ON VISUALISER:
+>Hover over point in 3d scatter to see the spectrum there
+>Hold right click to pan about
+>Hold left click to rotate
+>For more complex rotations when viewing in 3d, click the "orbit" button
+>To save one image, click the "save to png button"
+
+>2d map showing the number of components fitted per pixel is automatically
+made and loaded into visualser.
+
+
+>Clicking on a a point on the 3d scatter zooms into the 2d map at that position.
+ This zoom on click can be toggled off on the visualiser
+ NB: When viewed in world coords, the click to zoom on the 3d scatter is bugged.
+ It will sometimes not be able to show the zoomed in position. But, if you use
+ pixel coordinates, no buggy zoom occurs. Zoom-in is disabled for reprojections
+
+>If the error _reload-hash appears. Just wait a couple of seconds or refresh page
+
 """
 
 from nfitVisualiser import get_data, dash_visualise
@@ -10,10 +31,7 @@ from nfitVisualiser import conversion_utilities as util
 import numpy as np
 from astropy.io import fits
 
-from nfitVisualiser import make_maps
-
-
-#setting paths ====================================================================
+#setting paths
 DRIVE_PATH =
 PROJECT_PATH = DRIVE_PATH + 'andy_alma_test\\sdc24p489\\'
 
@@ -21,50 +39,48 @@ alma_setup = '12m+7m+tp'
 
 component_filename = PROJECT_PATH + 'SDC24p489-SPW07-N2Hp-uvb-merged-prim_maps.pkl'
 
-data_2d_paths = [component_filename]
+# If one 2d data file has header, list it first in 'data_2d_paths`
+data_2d_paths  = [component_filename]
 data_2d_labels = ['chisq_red']
 
 data_3d_path = PROJECT_PATH + 'SDC24p489-SPW07-N2Hp-uvb-merged-prim.fits'
+
 data_3d_fit_upper_lower_path = PROJECT_PATH + 'SDC24p489-SPW07-N2Hp-uvb-merged-prim_fit_cube.npy'
 
-
-#component data has no header so need to load that separatly
+#Fit params and the 2d data maps are not saved with headers so am loading it here
 with fits.open(data_3d_path) as hdu:
     header = hdu[0].header
 
+#Remove 3d info from header
 header = util.remove_axis_from_fits_header(header, 4)
 header = util.remove_axis_from_fits_header(header, 3)
 
-key_prefix = 'v_%d'
-max_components = None
-zlims=None # can set the range of velocities to initalise at
+key_prefix     = 'v_%d'   #Set to None if not needed, or exlcude from function call
+max_components = None     #Set to None if not needed, or exlcude from function call
+velo_start     = [45, 55] #Set to None if not wanted, or exlcude from function call
+start_spectrum = None     #Give [x, y] to initalise with a specific spectrum
+
 
 #initalise the objects and load in the data =======================================
 
-#Fitted parameters for 3d scatter. Loading from pickle so need header
+#Fit params for 3d scatter. Loading from pickle so need to manually provide header
 print('Loading component fits')
 data_obj = get_data.Mutlicomponent.load_data(
     filename=component_filename,
     header=header,
-    maximum_components=max_components,
-    key_prefix=key_prefix,
-    no_data_value=np.nan
+    maximum_components=max_components,  #Optional
+    key_prefix=key_prefix,              #Optional
+    no_data_value=np.nan                #Value where no fit was made. Default: nan
 )
-#2d map showing the number of components fitted per pixel
-# ncomp_2d = data_obj.get_flat2d_component_map()
-
-
-#first file contains header information so do not need to give header info,
-#if no files have header infomation, give it below. If one file contains
-#header information, list it first in the `data_2d_paths` list
 
 #2d data maps to view
+#The 2d files have header information, so need to manually provide it
 print('Loading and reprojecting 2d data maps')
 data_2d_obj = get_data.Data_2d(
     filenames=data_2d_paths,
     labels=data_2d_labels,
     header=header,
-    reproject_all=True
+    reproject_all=True #False means 2d maps will not be reprojected to galactic
 )
 
 
@@ -73,27 +89,28 @@ print('Loading in data cube and model spectra')
 real_data_obj = get_data.Data_3d.load_data(
     cube_filename=data_3d_path,
     model_cube_filename=data_3d_fit_upper_lower_path,
-    header=None,
-    data_is_freq=False,
-    rest_freq=None,
-    model_labels=['fit', 'lower', 'upper'],
-    in_kelvin=False
+    header=None,                                      #The data cube will have a header
+    data_is_freq=False,                               #Set True if data is in frequency
+    rest_freq=None,                                   #Give rest frequency if header does not contain it
+    model_labels=['fit', 'lower', 'upper'],           #Order of models in 4d hypercube
+    in_kelvin=False                                   #Set False if data in in Jy/beam
 )
 
 #The function only works for if the models params are labelled in a dictionary
-#with a label for each component, and the label names assend as
-#v_1, v_2 v_3 etc and errors are v_1_err_down, v_1_err_up etc
-fit_lower_upper = get_data.all_fit_param_cubes(component_filename, ['sigma_%d', 'tau_%d', 'tex_%d', 'v_%d'], err_suffix=['_err_down', '_err_up'])
+#with a label for each component, and the label names accend as
+#v_1, v_2 v_3 etc and errors are v_1_err_down, v_1_err_up etc << dont use these errors
+single_model_fits = get_data.all_fit_param_cubes(component_filename, ['sigma_%d', 'tau_%d', 'tex_%d', 'v_%d'])
 
 
-#initalise the visualiser parameters
+##initalise the visualiser parameters
 print('Initalising visualiser')
 vis = dash_visualise.Visualiser(
-    data_obj=data_obj,
-    data_2d_obj=data_2d_obj,
-    real_data_obj=real_data_obj,
-    single_fits = fit_lower_upper,
-    zlims=zlims
+    data_obj=data_obj,             #peak velocities, xy, RA-DEC, GLON-GLAT coords
+    data_2d_obj=data_2d_obj,       #2d data map
+    real_data_obj=real_data_obj,   #datacubes
+    single_fits=single_model_fits, #datacube model
+    zlims=velo_start,
+    inital_view_spectrum=start_spectrum
 )
 
 # #run the visuliser: copy the address into the url. Works with chrome. Hope it
